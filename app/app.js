b00aae897ba0c4cce60e1d87a5ee9956fba76a78
toran billups <toranb@gmail.com>
2015-06-23 22:08:58 -0500

    updated for glimmer (no hack required anymore)

--- app/app.js
+++ app/app.js
@@ -3,20 +3,6 @@ import Resolver from 'ember/resolver';
 import loadInitializers from 'ember/load-initializers';
 import config from './config/environment';
 
-var custom = function(params, hash, options, env) {
-  var view = env.data.view;
-  var property = "main";
-  if (params.length > 0) {
-      property = params[0];
-  }
-  var viewClass = view.container.lookupFactory('view:-outlet');
-  hash._outletName = property;
-  options.helperName = options.helperName || 'outlet';
-  return env.helpers.view.helperFunction.call(this, [viewClass], hash, options, env);
-};
-
-Ember.HTMLBars._registerHelper('custom-outlet', custom);
-
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
 var App = Ember.Application.extend({
